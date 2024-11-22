ec00311c80c8d84e99f665d05d03f1feca217fcf
Guitar67160065 <67160065@kmitl.ac.th>
2024-11-22 03:33:58 +0000

    asd

---
+++ cell01/ex00/name.py
@@ -0,0 +1,5 @@
+first_name = "วิชัย"
+last_name = "ศรีนาค"
+
+print(first_name)
+print(last_name)
---
+++ cell04/ex00/upcase_it.py
@@ -0,0 +1,4 @@
+
+word = input("Give me a word: ")
+uppercase_word = word.upper()
+print(uppercase_word)
